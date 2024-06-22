c422c3d2354c56d9d6ef3d569c8c2f5acddeff12
JNeo77 <lifted_2000@yahoo.com>
2024-06-21 21:37:59 -0400

    Add functionality for grid size and clear buttons
    Remove the existing grid and create a new grid when user clicks Change
    Grid Size button and enters a number.
    If a number is greater than 100 or less than or equal to zero, alert an
    error.
    Clear color from the grid squares when user clicks the Reset button.

--- scripts/script.js
+++ scripts/script.js
@@ -1,4 +1,6 @@
 const container = document.querySelector(".container");
+const gridButton = document.querySelector(".grid-btn");
+const resetButton = document.querySelector(".reset-btn");
 
 function createGrid (count = 16) {
   for (let i = 0; i < count; i++) {
@@ -16,6 +18,33 @@ function createGrid (count = 16) {
   }
 }
 
+function deleteGrid () {
+  let containerChild = container.lastElementChild;
+  while (containerChild) {
+    container.removeChild(containerChild);
+    containerChild = container.lastElementChild;
+  } 
+}
 
-createGrid();
+document.addEventListener("DOMContentLoaded", () => {
+  createGrid();
+});
 
+gridButton.addEventListener("click", () => {
+  const size = Number(prompt("Enter size of the grid (max is 100)."));
+  if (size > 100) {
+    alert(`Maximum size is 100x100 squares. You entered ${size}. Please try again!`);
+  } else if (size <= 0) {
+    alert(`Must be a number greater than 0. You entered ${size}. Please try again!`);
+  } else {
+    deleteGrid();
+    createGrid(size);
+  }
+});
+
+resetButton.addEventListener("click", () => {
+  const gridSquare = document.querySelectorAll(".square");
+  for (let i = 0; i < gridSquare.length; i++) {
+    gridSquare[i].classList.remove("color");
+  }
+});
